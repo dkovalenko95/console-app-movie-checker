45f2674aa76866a9ef78aaab8f8b4a6f1948cda3
Dmytro <dkovalenko2425@gmail.com>
2021-09-06 18:28:31 +0300

    app refactoring

--- js/script.js
+++ js/script.js
@@ -1,86 +1,110 @@
 'use strict';
-// USING BASIC FUNCTIONS
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many movies have you already watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many movies have you already watched?', '');
-    }
-}
-start();
-
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
-    genres: [],
-    privat: false
+    ganres: [],
+    privat: false,
+
+    start: function() {
+        personalMovieDB.count = +prompt('How many movies have you already watched?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many movies have you already watched?', '');
+        }
+    },
+
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Name one of the last movies you watched', ''),
+                  b = prompt('How did you rate the movie?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+                personalMovieDB.movies[a] = b;
+                alert('Done!');
+                console.log('Done');
+            } else {
+                alert('Error!');
+                console.log('Error!!!');
+                i--;
+            }    
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert('You watched quite a few movies');
+            console.log('You watched quite a few movies');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert('You are a classic viewer');
+            console.log('You are a classic viewer');
+        } else if (personalMovieDB.count >= 30) {
+            alert('You are a real movie fan');
+            console.log('You are a real movie fan');
+            
+        } else {
+            alert('You have not answer the question!');
+            console.log('You have not answer the question!');
+        }
+    },
+
+    writeYourGanres: function() {
+        for (/* let i = 1; i <= 3; i++ */ let i = 1; i < 2; i++) {
+            /* const ganre = prompt(`Your favorite ganre is ${i}`);
+            personalMovieDB.ganres[i - 1] = ganre; */
+            
+            // ОСНОВНОЙ ВАРИАНТ
+            /* let ganre = prompt(`Your favorite ganre is ${i}`);
+            
+            if (ganre == '' || ganre == null) {
+                console.log('You wrote wrong data or left this field empty');
+                i--;
+            } else {
+                personalMovieDB.ganres[i - 1] = ganre;
+            } */
+
+            // ONE MORE VERSION OF REALIZATION
+            let ganres = prompt(`Введите ваши любимые жанры через запятую`).toLocaleLowerCase(); 
+            /*страхует от ошибок ввода, переводит в нижний регистр*/
+
+            if (ganres == '' || ganres == null) {
+                console.log('You wrote wrong data or left this field empty');
+                i--;
+            } else {
+                personalMovieDB.ganres = ganres.split(', ');
+
+                personalMovieDB.ganres.sort();
+            } 
+        }
+
+        /* При выводе данных методом "sort()" в приоретете данные в большом регистре*/
+
+        // console.log(typeof(personalMovieDB.ganres));
+
+        personalMovieDB.ganres.forEach((item, i) => {
+            console.log(`Favorite ganre ${i + 1} is ${item}`);
+        });
+    },
+
+    showMyDB: function(hidden) {
+        /* if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log('Privat information');
+        } */
+        if(!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }
 };
 
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Name one of the last movies you watched', ''),
-              b = prompt('How did you rate the movie?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-            alert('Done!');
-            console.log('Done');
-        } else {
-            alert('Error!');
-            console.log('Error!!!');
-            i--;
-        }    
-    }
-}
 
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('You watched quite a few movies');
-        console.log('You watched quite a few movies');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('You are a classic viewer');
-        console.log('You are a classic viewer');
-    } else if (personalMovieDB.count >= 30) {
-        alert('You are a real movie fan');
-        console.log('You are a real movie fan');
-        
-    } else {
-        alert('You have not answer the question!');
-        console.log('You have not answer the question!');
-    }
-}
-
-
-function showMyDB() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('Privat information');
-    }
-}
-showMyDB();
-// one more variation of realization
-function showMyDB2(hidden) {
-    if(!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-// showMyDB2(personalMovieDB.privat);
-
-
-function writeYourGanres() {
-    for (let i = 1; i <= 3; i++) {
-        const ganre = prompt(`Your favorite ganre is ${i}`);
-        personalMovieDB.genres[i - 1] = ganre;
-
-        // second shorter variation
-        // personalMovieDB.genres[i - 1] = prompt(`Your favorite ganre is ${i}`);
-    }
-}
-writeYourGanres();

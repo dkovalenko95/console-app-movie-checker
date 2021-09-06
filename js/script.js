ac9b3970789805bd650da19e8a4265c214e77b59
Dmytro <dkovalenko2425@gmail.com>
2021-09-06 18:10:47 +0300

    used basic functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict';
+// USING BASIC FUNCTIONS
+let numberOfFilms;
 
-const numberOfFilms = +prompt('How many movies have you already watched?', '');
+function start() {
+    numberOfFilms = +prompt('How many movies have you already watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies have you already watched?', '');
+    }
+}
+start();
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,26 +21,26 @@ const personalMovieDB = {
 };
 
 
-/* const a = prompt('Name one of the last movies you watched', ''),
-      b = prompt('How did you rate the movie?', ''),
-      c = prompt('Name one of the last movies you watched', ''),
-      d = prompt('How did you rate the movie?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Name one of the last movies you watched', ''),
+              b = prompt('How did you rate the movie?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+            personalMovieDB.movies[a] = b;
+            alert('Done!');
+            console.log('Done');
+        } else {
+            alert('Error!');
+            console.log('Error!!!');
+            i--;
+        }    
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; */
-/* put data in object */
-/* console.log(personalMovieDB); */
 
-
-// QUESTION & ANSWER TASK
-for (let i = 0; i < 1; i++ ) {
-    const numberOfFilms = +prompt('How many movies have you already watched?', '');
-        console.log(numberOfFilms);
-    if (numberOfFilms == '' || numberOfFilms == null) {
-        alert('Please, answer the question by using number');
-        i--;
-
-    } else if (personalMovieDB.count < 10) {
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
         alert('You watched quite a few movies');
         console.log('You watched quite a few movies');
     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -46,55 +56,31 @@ for (let i = 0; i < 1; i++ ) {
     }
 }
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Name one of the last movies you watched', ''),
-          b = prompt('How did you rate the movie?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
-        alert('Done!');
-        console.log('Done');
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
     } else {
-        alert('Error!');
-        console.log('Error!!!');
-        i--;
-    }    
-}
-
-
-// SAME FEATURE WITH 'WHILE'&'DO' LOOPS INSTEAD OF 'FOR'
-/* let i = 0;
-while (i < 2) {
-    const a = prompt('Name one of the last movies you watched', ''),
-          b = prompt('How did you rate the movie?', '');
-    i++;
-    if  (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
-        alert('Done');
-        console.log('Done');
-    } else {
-        alert('Error!');
-        console.log('Error!');
-        i--;
-    }
-} */
-
-
-/* let i = 0;
-do {
-    const a = prompt('Name one of the last movies you watched', ''),
-          b = prompt('How did you rate the movie?', '');
-    i++;
-    if  (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
-        alert('Done');
-        console.log('Done');
-    } else {
-        alert('Error!');
-        console.log('Error!');
-        i--;
+        console.log('Privat information');
     }
 }
-while (i < 2); */
+showMyDB();
+// one more variation of realization
+function showMyDB2(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+// showMyDB2(personalMovieDB.privat);
 
-console.log(personalMovieDB);
+
+function writeYourGanres() {
+    for (let i = 1; i <= 3; i++) {
+        const ganre = prompt(`Your favorite ganre is ${i}`);
+        personalMovieDB.genres[i - 1] = ganre;
+
+        // second shorter variation
+        // personalMovieDB.genres[i - 1] = prompt(`Your favorite ganre is ${i}`);
+    }
+}
+writeYourGanres();
